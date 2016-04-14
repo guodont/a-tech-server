901d2f8649e80a26636f48028297f432f46c1dbe
guodont <guodont@163.com>
2016-04-14 20:30:48 +0800

    userService 测试类

--- admin/test/ordercenter/services/UserServiceTest.java
+++ admin/test/ordercenter/services/UserServiceTest.java
@@ -9,34 +9,38 @@ import org.junit.Test;
 import java.util.List;
 
 /**
- *
  * 测试 UserService
- *
+ * <p>
  * Created by guodont on 16/4/13.
  */
 public class UserServiceTest extends BaseTest {
 
-    User initUser() {
+    /**
+     * 初始化model数据
+     *
+     * @return
+     */
+    User initUser(int i) {
         User user = new User();
-        user.setAddress("山西农业大学");
+        user.setAddress("山西农业大学" + i);
         user.setAvatar("image/avatar.png");
-        user.setEmail("test@qq.com");
-        user.setName("user01");
+        user.setEmail("test" + i + "@qq.com");
+        user.setName("user" + i);
         user.setPassword("123");
-        user.setPhone("18900009999");
-        user.setRealName("real");
+        user.setPhone("18900000099");
+        user.setRealName("real_name" + i);
         user.setUserType(UserType.PUBLIC);
         user.setLastLoginTime(new DateTime());
         user.setRegisterTime(new DateTime());
         return user;
     }
 
+
     void prepareUser(int size) {
         doInTransaction(em -> {
             em.createQuery("delete from User").executeUpdate();
-            //创建订单
             for (int i = 0; i < size; i++) {
-                User user = initUser();
+                User user = initUser(i);
                 em.persist(user);
             }
             return null;
